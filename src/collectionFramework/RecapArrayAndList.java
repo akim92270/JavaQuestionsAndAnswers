c1c7c0d3727d0ffb5aa4c01c6277646910bd2f43
akim92270 <akim.92270@gmail.com>
2022-04-24 11:56:09 -0500

    finished recapArrayAndList

--- src/collectionFramework/RecapArrayAndList.java
+++ src/collectionFramework/RecapArrayAndList.java
@@ -1,10 +1,9 @@
 package collectionFramework;
 
+import sun.awt.image.ImageWatched;
+
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class RecapArrayAndList {
     public static void main(String[] args) {
@@ -22,10 +21,53 @@ public class RecapArrayAndList {
         String[] cities = {"Tokyo", "Berlin", "Oslo", "Denver", null, null};
         System.out.println(Arrays.toString(cities));
 
-        System.out.println("\n------ array-----\n");
+        System.out.println("\n------ String arrayList-----\n");
 
         ArrayList<String> names1 = new ArrayList<>();
         List<String> names2 = new ArrayList<>();
 
+        /*
+        List: ArrayList - Vector - LinkedList
+         */
+
+        System.out.println("\n------String ArrayList-----\n");
+        ArrayList<String> cities2 = new ArrayList<>();
+        Collections.addAll(cities2, cities);
+        cities2.add("Tokyo");
+
+        System.out.println(cities2);
+        System.out.println(cities2.get(3)); // Denver
+
+
+
+        System.out.println("\n------ String Vector ArrayList-----\n");
+        Vector<String> names = new Vector<>();
+        names.add(null);
+        names.add(null);
+        names.add(null);
+        names.add("Regina");
+        names.add("Regina");
+        names.add("Data");
+
+        System.out.println(names.size());
+        names.forEach(System.out::println);
+        System.out.println("Name at index of 5 = " + names.get(5));
+
+        System.out.println("\n------String LinkedList-----\n");
+        LinkedList<String> objects1 = new LinkedList<>();
+        objects1.add("Phone");
+        objects1.add("Computer");
+        objects1.add("Airpods");
+        objects1.add("Screen");
+        objects1.add("Screen");
+        objects1.add(null);
+        objects1.add(null);
+        objects1.add(null);
+        objects1.add(null);
+
+        System.out.println(objects1.getLast());
+        System.out.println(objects1);
+        System.out.println(objects1.get(2)); //Airpod
+        System.out.println(objects1.stream().filter(Objects::isNull).count()); // 4
     }
 }
