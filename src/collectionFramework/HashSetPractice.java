e28ef6d2cc6f2914c5f872dd6949961fa3d9319e
akim92270 <akim.92270@gmail.com>
2022-04-24 12:10:51 -0500

    added HashSet

---
+++ src/collectionFramework/HashSetPractice.java
@@ -0,0 +1,27 @@
+package collectionFramework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetPractice {
+    public static void main(String[] args) {
+        /*
+        How to create a HashSet
+         */
+
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(5);
+        numbers.add(10);
+        numbers.add(10);
+        numbers.add(10);
+        numbers.add(-3);
+        numbers.add(0);
+        numbers.add(-7);
+        numbers.add(-55);
+        System.out.println(numbers);
+
+
+
+
+    }
+}
