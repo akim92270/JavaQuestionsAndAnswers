fd69c751a6597414af5faa500efc504081924ebc
akim92270 <akim.92270@gmail.com>
2022-04-24 13:19:16 -0500

    exercises TreeSetPractice

--- src/collectionFramework/TreeSetPractice.java
+++ src/collectionFramework/TreeSetPractice.java
@@ -1,4 +1,25 @@
 package collectionFramework;
 
+import java.util.TreeSet;
+
 public class TreeSetPractice {
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(5);
+
+        System.out.println(numbers);
+
+        TreeSet<String> names = new TreeSet<>();
+        names.add("John");
+        names.add("Joe");
+        names.add("Alex");
+        names.add("ali");
+
+        System.out.println(names);
+
+
+    }
 }
