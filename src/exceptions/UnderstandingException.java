7b73236b9f6ce45a4ca95a24c55efa03e8a6d79f
akim92270 <akim.92270@gmail.com>
2022-04-26 18:25:49 -0500

    added exception example

--- src/exceptions/UnderstandingException.java
+++ src/exceptions/UnderstandingException.java
@@ -3,10 +3,20 @@ package exceptions;
 public class UnderstandingException {
     public static void main(String[] args) {
 
+        /*
+        Unchecked exceptions are the ones you get when you run a program
+        Unchecked exceptions = Runtime exceptions
+         */
+
         String name = "John";
 
-        System.out.println(name.charAt(10));
-
+        //How to handle the exception
+        try {
+            System.out.println(name.charAt(10)); //StringIndexOutOfBoundsException
+        }
+        catch (StringIndexOutOfBoundsException exception){
+            System.out.println("An exception is caught");
+        }
         System.out.println("End of the program!");
     }
 }
