97e321b190567e7db552b2aab810551961bacb52
akim92270 <akim.92270@gmail.com>
2022-04-25 19:02:44 -0500

    added 2nd problem

---
+++ src/recursion/_02_ProductOfNumbers.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class _02_ProductOfNumbers {
+    /*
+    Write a public static recursive method that takes an int n argument and returns the product
+    of all numbers starting from 1 to int n.
+    Example:
+    if n = 5
+    1*2*3*4*5 = 120
+
+    result - 120
+     */
+
+    public static int recursiveMultiply(int n){
+        if(n > 0) return n * recursiveMultiply(n-1);
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recursiveMultiply(6));
+    }
+}
