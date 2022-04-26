6b1bea26d0a226c7334d076a9121a261ad37c865
akim92270 <akim.92270@gmail.com>
2022-04-26 18:21:05 -0500

    added exception package

---
+++ src/exceptions/UnderstandingException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class UnderstandingException {
+    public static void main(String[] args) {
+
+        String name = "John";
+
+        System.out.println(name.charAt(10));
+
+        System.out.println("End of the program!");
+    }
+}
