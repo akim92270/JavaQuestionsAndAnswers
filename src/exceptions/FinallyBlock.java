8e5a9cc36b440b89f5695e214c93648c10ba1113
akim92270 <akim.92270@gmail.com>
2022-04-26 19:40:33 -0500

    added Finally

---
+++ src/exceptions/FinallyBlock.java
@@ -0,0 +1,26 @@
+package exceptions;
+
+import java.util.ArrayList;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+
+        ArrayList<String> objects = new ArrayList<>();
+        objects.add("Computer");
+        objects.add("Mouse");
+        objects.add("MousePad");
+
+        try {
+            for (String object : objects) {
+                objects.remove(object); // ConcurrentModificationException
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            //It is not handling the exception but still runs a block of code even though there is an exception
+            System.out.println("Here is the report! We report the exception here!");
+        }
+        System.out.println("End of program!");
+    }
+}
