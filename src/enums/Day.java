db804de2b881f0898e7d1838ed6fe673fd180819
akim92270 <akim.92270@gmail.com>
2022-04-30 11:22:20 -0500

    enums

---
+++ src/enums/Day.java
@@ -0,0 +1,12 @@
+package enums;
+
+public class Day {
+    public static final String monday = "MONDAY";
+    public static final String tuesday = "TUESDAY";
+    public static final String wednesday = "WEDNESDAY";
+    public static final String thursday = "THURSDAY";
+    public static final String friday = "FRIDAY";
+    public static final String saturday = "SATURDAY";
+    public static final String sunday = "SUNDAY";
+
+}
---
+++ src/enums/Test.java
@@ -0,0 +1,9 @@
+package enums;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(Day.monday); //MONDAY
+        System.out.println(Day.sunday); //SUNDAY
+    }
+}
